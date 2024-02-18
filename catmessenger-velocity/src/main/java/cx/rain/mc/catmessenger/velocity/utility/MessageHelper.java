437d69dcddf82f25f399b389f96888483ce9d4c5
秋雨落 <i@rain.cx>
2024-02-18 16:20:30 +0800

    :sparkles: Little changes.
    Signed-off-by: 秋雨落 <i@rain.cx>

--- catmessenger-velocity/src/main/java/cx/rain/mc/catmessenger/velocity/utility/MessageHelper.java
+++ catmessenger-velocity/src/main/java/cx/rain/mc/catmessenger/velocity/utility/MessageHelper.java
@@ -7,14 +7,12 @@ import cx.rain.mc.catmessenger.message.type.TextMessage;
 public class MessageHelper {
     public static AbstractMessage buildServerOnlineMessage() {
         var message = new TextMessage("服务器上线了");
-        message.setBold(true);
         message.setColor(MessageColor.YELLOW);
         return message;
     }
 
     public static AbstractMessage buildServerOfflineMessage() {
         var message = new TextMessage("服务器离线了");
-        message.setBold(true);
         message.setColor(MessageColor.YELLOW);
         return message;
     }
